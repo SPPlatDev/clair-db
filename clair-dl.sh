0771c272133e77894ac3c438aa90fc1882b43cda
nic <nicolas.bush@fujitsu.co.uk>
2025-01-07 16:11:01 +0000

    undelete files

---
+++ clair-dl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+docker run --rm -d --link postgres:postgres -e PGPASSWORD=password $POSTGRES_IMAGE pg_isready -U postgres -h postgres
+docker run -d --name clair --link postgres:postgres $CLAIR_LOCAL_SCAN_IMAGE
+
+sleep 10
+
